7c4b4e9f59f0831c309e2decc26b05069f08edda
Liu Fangxu <l136665@163.com>
2023-07-10 09:44:19 +0800

    add gitignore

--- build/OBJ_DIR/main.d
+++
@@ -1,2 +0,0 @@
-main.o: /home/ysyx-chisel/learnchisel/lc3lab/decoder/sim_c/main.cc \
- /home/ysyx-chisel/learnchisel/lc3lab/decoder/sim_c/include/sim.h
--- build/OBJ_DIR/sim.d
+++
@@ -1,7 +0,0 @@
-sim.o: /home/ysyx-chisel/learnchisel/lc3lab/decoder/sim_c/sim.cc \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- /usr/local/share/verilator/include/verilated_config.h \
- /usr/local/share/verilator/include/verilated_types.h \
- /usr/local/share/verilator/include/verilated_funcs.h Vtop.h \
- /home/ysyx-chisel/learnchisel/lc3lab/decoder/sim_c/include/sim.h
