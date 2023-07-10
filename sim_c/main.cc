6178a2622be9eadfe019124141de5aba5a75643c
Liu Fangxu <l136665@163.com>
2023-07-10 13:04:28 +0800

    删除冗余文件，完成框架搭建

--- sim_c/main.cc
+++ sim_c/main.cc
@@ -1,7 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
-
 #include "sim.h"
 
 int main(int argc, char *argv[]) {
--- sim_c/sim.cc
+++ sim_c/sim.cc
@@ -1,4 +1,3 @@
-#include <memory>
 #include <iostream>
 #include <bitset>
 
